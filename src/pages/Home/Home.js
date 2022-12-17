7bf9003026585dc9990235e2c050e3ab7eaab2c4
Phelipe Victor <phelipevictor5@gmail.com>
2022-12-17 11:04:01 -0300

    homepage link fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
 
 import '../Home/Home.css'
 
@@ -11,8 +12,8 @@ const Home = () => {
         <h1 className='title1'>Welcome to the Workout Aplication</h1>
         <h3 className='title3'>Please create an account to use our application</h3>
         <div className='links'>
-        <a href="/Signup"> Click here to Sign-up!</a>
-        <a href="/Login"> Click here to Login-in!</a>
+        <Link to="/Signup"> Click here to Sign-up!</Link>
+        <Link to="/Login"> Click here to Login-in!</Link>
         </div>
     </div>
     </div>
